2b664bdaaac0d4f8bda61580f906a09195a5fab4
lburtscher <lukas.burtscher@check24.at>
2024-01-26 17:02:43 +0100

    add logistic regression model

--- application/app.py
+++ application/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
+import pickle
 
 # sys.path.insert(0, "../scripts")
 
@@ -39,6 +40,31 @@ def get_recommendation(year:int, country:str, isin:str):
 
     return transform_output(X)
 
+def get_recommendation_log(year:int, country:str, isin:str):
+    # print(os.getcwd())
+    # get the company from the FDI dataframe
+    df_fdi_filtered = df_fdi[(df_fdi['year'] == year) & (df_fdi['cowc_source'] == country) & (
+                df_fdi['isin'] == isin)].iloc[0:1]
+    # get all countries the companys source country invested to in that year
+    df_country_grouped = df_country[(df_country['year'] == year) & (df_country['cowc_source'] == country)] \
+        .groupby('cowc_dest').first().reset_index()
+
+    merger = Merger(df_country=df_country_grouped, df_fdi=df_fdi_filtered)
+    X = merger.get_result()
+    df_model_X = df_model.drop(['invest_actual'], axis=1)
+    # get all the columns from the model data frame
+    cols = set(df_model_X.columns) - set(X.columns)
+    for col in cols:
+        X[col] = 0
+    # only take common columns in same order
+    X = X[df_model_X.columns]
+    with open('../data/model/log_reg_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    y = model.predict_proba(X)
+    X['label'] = y[:, 1]
+
+    return transform_output(X)
+
 def transform_output(df_raw:pd.DataFrame):
     # Backtransform 'cowc_dest' one-hot encoded columns
     cols = [col for col in df_raw.columns if 'cowc_dest' in col]
@@ -68,7 +94,18 @@ if year:
 
 if st.button('Get Recommendations'):
     res = get_recommendation(year, country, isin)
-    st.dataframe(res.head())
+    res_log = get_recommendation_log(year, country, isin)
+    # Create two columns
+    col1, col2 = st.columns(2)
+
+    # Display the dataframes in the columns with the respective headings
+    with col1:
+        st.write("Neural Network Suggestion")
+        st.dataframe(res.head())
+
+    with col2:
+        st.write("Logistic Regression Suggestion")
+        st.dataframe(res_log.head())
 
 
 
@@ -86,4 +123,6 @@ df_fdi_year_cowc_source = df_fdi_year[df_fdi_year['cowc_source'].isin(available_
 if __name__ == '__main__':
 
     g = get_recommendation(2009, 'DE', 'DE0005297204')
+    g1 = get_recommendation_log(2009, 'DE', 'DE0005297204')
     print(g.head())
+    print(g1.head())
